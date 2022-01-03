99bc89a0f42443d6ea5c07d836bd5b3a410d06e9
dosepix <schm.seb@gmail.com>
2022-01-03 23:42:24 +0100

    Added GAN to PG

--- scripts/wagara.js
+++ scripts/wagara.js
@@ -1,17 +1,35 @@
 import * as tf from 'https://cdn.skypack.dev/@tensorflow/tfjs';
 
 const pattern_canvas = document.getElementById('pattern-canvas');
-const model = await tf.loadLayersModel('./assets/wagara_model/model.json');
 const generate_button = document.getElementById('generate-button');
 const use_gradient = document.getElementById('use-gradient');
+const use_quantize = document.getElementById('use-quantize');
+use_quantize.checked = true;
 const quant_slider = $( "#slider-range" );
 const random_button = document.getElementById('random-button');
+const model_select = document.getElementById('model-select');
 
 const color_one = document.getElementById('color-one');
 const color_two = document.getElementById('color-two');
 const color_three = document.getElementById('color-three');
 
 const large = true;
+var gan = false;
+const model_gan = await tf.loadLayersModel('./assets/wagara_gan/model.json');
+const model_vae = await tf.loadLayersModel('./assets/wagara_model/model.json');
+var model = model_vae;
+model_select.value = 'VAE';
+
+model_select.addEventListener('change', () => {
+    if(model_select.value == 'GAN') {
+        gan = true;
+        model = model_gan;
+    } else {
+        gan = false;
+        model = model_vae;
+    }
+});
+
 const w = 96, h = 96;
 if (large) {
     pattern_canvas.width = w * 10;
@@ -23,8 +41,16 @@ if (large) {
 
 function random_vector() {
     let random_vector = [];
-    for (let i = 0; i < 10; i++) {
-        random_vector.push( 5 * (2 * Math.random() - 1) );
+    let size, fac;
+    if (gan) {
+        size = 100;
+        fac = 10;
+    } else {
+        size = 10;
+        fac = 5;
+    }
+    for (let i = 0; i < size; i++) {
+        random_vector.push( fac * (2 * Math.random() - 1) );
     }
     return random_vector;
 }
@@ -66,7 +92,18 @@ function set_colors(pred, quant_bins, colors) {
     return buffer;
 }
 
-function set_colors_large(pred, quant_bins, colors, use_grad) {
+function color_mix(color1, color2, weight) {
+    let w1 = weight;
+    let w2 = 1 - w1;
+    let rgb = {
+        r: color1.r * w1 + color2.r * w2,
+        g: color1.g * w1 + color2.g * w2,
+        b: color1.b * w1 + color2.b * w2,
+    };
+    return rgb;
+}
+
+function set_colors_large(pred, quant_bins, colors, use_grad, use_quant=false) {
     var buffer = new Uint8ClampedArray(w * 10 * h * 10 * 4);
 
     for (let i = 0; i < 10; i++) {
@@ -75,21 +112,32 @@ function set_colors_large(pred, quant_bins, colors, use_grad) {
             let pos_large = i * w + j * (10 * w * h);
             for (let y = 0; y < w; y++) {
                 for (let x = 0; x < h; x++) {
-                    let c = p[y][x];
-                    c = quantize(c, quant_bins);
                     let pos = (pos_large + y * (10 * w) + x) * 4;
-
                     let grad;
                     if (use_grad) {
                         grad = (j * h + y) / (10 * h) + 0.3;
                     } else {
                         grad = 1;
                     }
-                    // grad = Math.max( 0, Math.min( grad, 1) );
-                    buffer[pos] = colors[c].r * grad;
-                    buffer[pos + 1] = colors[c].g * grad;
-                    buffer[pos + 2] = colors[c].b * grad;
-                    buffer[pos + 3] = 255;
+
+                    let c = p[y][x];
+                    let q = quantize(c, quant_bins);
+                    let color;
+                    if (use_quant) {
+                        color = colors[q];
+                    } else {
+                        let k = q - 1;
+                        if (k < 0) {
+                            color = colors[q];
+                        } else {
+                            color = color_mix(colors[k], colors[q], 0.5 * (c + 1));
+                        }
+                    }
+
+                    buffer[pos] = color.r * grad;
+                    buffer[pos + 1] = color.g * grad;
+                    buffer[pos + 2] = color.b * grad;
+                    buffer[pos + 3] = 255;    
                 }
             }        
         }
@@ -106,16 +154,24 @@ function predict() {
 
 function predict_large() {
     pred = [];
+    let fac, num;
+    if (gan) {
+        num = 100;
+        fac = 100;
+    } else {
+        num = 10;
+        fac = 5;
+    }
     tf.tidy(() => {
         let rand_vec = random_vector();
-        let i_rand = Math.floor( Math.random() * 10 );
-        let j_rand = Math.floor( Math.random() * 10 );
+        let i_rand = Math.floor( Math.random() * num );
+        let j_rand = Math.floor( Math.random() * num );
 
         for (let i = 0; i < 10; i++) {
-            rand_vec[i_rand] = (2 * i / 10 - 1) * 5;
+            rand_vec[i_rand] = (2 * i / 10 - 1) * fac;
             let res_row = [];
             for (let j = 0; j < 10; j++) {
-                rand_vec[j_rand] = (2 * j / 10 - 1) * 5;
+                rand_vec[j_rand] = (2 * j / 10 - 1) * fac;
                 let p = model.predict( tf.stack([rand_vec]) ).arraySync()[0];
                 res_row.push( p.slice() );
             }
@@ -138,7 +194,7 @@ function make_image() {
 
     let buffer;
     if (large) {
-        buffer = set_colors_large(pred, quant_bins, colors, use_gradient.checked);
+        buffer = set_colors_large(pred, quant_bins, colors, use_gradient.checked, use_quantize.checked);
     } else {
         buffer = set_colors(pred, quant_bins, colors);
     }
@@ -177,6 +233,7 @@ random_button.addEventListener('click', () => {
 })
 
 use_gradient.addEventListener('change', make_image);
+use_quantize.addEventListener('change', make_image);
 
 // jQuery range slider
 $( function() {
